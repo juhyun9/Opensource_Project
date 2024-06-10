f418c12fe7f840d8381ad00caf2d39cb3052d1e0
carol7378 <101542336+carol7378@users.noreply.github.com>
2024-06-10 12:22:58 +0900

    Change data.js and start.js

--- static/start.js
+++ static/start.js
@@ -5,37 +5,38 @@ const result = document.querySelector("#result");
 const endPoint = 3; //질문의 총 개수
 const select = [0, 0, 0, 0, 0, 0, 0, 0];
 
-const qnaList = require("./qna.json");
-const infoList = require("./info.json");
-const infoName = Object.keys(infoList);
-
 function calResult() {
   console.log(select);
   let res = 0;
-  for (i = 0; i < 5; i++) {
+  for (i = 0; i < 4; i++) {
     let a = select[i * 2] - select[i * 2 + 1]; //짝수 ESTJ 홀수 INFP
+    console.log(a);
     if (a >= 0) {
-      res += res * 2 + 1;
+      res = res * 2 + 1;
+    } else {
+      res = res * 2;
     }
+    console.log(res);
   }
-  return infoName[res];
+  console.log(res);
+  return res;
 }
 
 function setResult() {
-  let point = calResult(); //'쥐'
+  let point = calResult(); //0
   const resultName = document.querySelector(".resultname");
-  resultName.innerHTML = infoList[point].name;
+  resultName.innerHTML = infoList[infoName[point]].name;
 
   var resultImg = document.createElement("img");
   const imgDiv = document.querySelector("#resultImg");
-  var imgURL = "img/" + point + ".png";
+  var imgURL = "static/img/image-" + point + ".png";
   resultImg.src = imgURL;
   resultImg.alt = point;
   resultImg.classList.add("img-fluid");
   imgDiv.appendChild(resultImg);
 
   const resultDesc = document.querySelector(".resultDesc");
-  resultDesc.innerHTML = infoList[point].desc;
+  resultDesc.innerHTML = infoList[infoName[point]].desc;
 }
 
 function goResult() {
@@ -68,8 +69,8 @@ function ImageFadeOut(qIdx, idx) {
       return;
     } else {
       setTimeout(() => {
-        var target = qnaList[qIdx].a[idx].type;
-        for (let i = 0; i < target.length; i++) {
+        var target = qnaList[qIdx].a["Y"];
+        for (let i = 0; i < 8; i++) {
           select[target[i]] += 1;
         }
         goNext(++qIdx);
@@ -84,7 +85,7 @@ function goNext(qIdx) {
 
   var left = document.querySelector(".leftImage");
   var right = document.querySelector(".rightImage");
-  var qnaURL = "./img/question/";
+  var qnaURL = "./static/img/question/";
   left.src = qnaURL + qIdx + "-A.png";
   right.src = qnaURL + qIdx + "-B.png";
 
@@ -133,3 +134,14 @@ function begin() {
     goNext(qIdx);
   }, 450);
 }
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOMContentLoaded");
+  // 시작 버튼에 클릭 이벤트 리스너 등록
+  const startButton = document.querySelector("#startButton");
+  startButton.addEventListener("click", function (event) {
+    console.log("start button clicked");
+    event.preventDefault(); // 폼의 기본 동작을 중단시킵니다.
+    const startForm = document.querySelector("#startForm");
+    begin();
+  });
+});
