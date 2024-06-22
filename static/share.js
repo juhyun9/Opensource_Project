78e2cbc5797c1fb63c0cc261f35387265b77df73
yvonnekwak <yebonkwak915@gmail.com>
2024-06-22 13:33:18 +0900

    이름 변경
    기존 이름 변경

--- static/share.js
+++ static/share.js
@@ -3,7 +3,7 @@ const url = 'https://twelvelovetype.netlify.app/';
 function setShare(){
   var resultImg = document.querySelector('#resultImg');
   var resultAlt = resultImg.firstElementChild.alt;
-  const shareTitle = '십이간지 연애유형 결과';
+  const shareTitle = '올레코스 유형 테스트 결과';
   const shareDes = infoList[resultAlt].name;
   const shareImage = url + 'img/image-' + resultAlt + '.png';
   const shareURL = url + 'page/result-' + resultAlt + '.html';
