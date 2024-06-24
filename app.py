c69ded2e7594e834ea032fbec77a6780d6f1ca2f
juhyun9 <990001@stu.jejunu.ac.kr>
2024-06-24 11:23:08 +0900

    유형 알고리즘

--- app.py
+++ app.py
@@ -8,12 +8,6 @@ def home():
     paragraph = "<나만의 MBTI 사이트입니다!> 와 같은 한 줄 설명해주세요."
     return render_template('home.html', title=title, paragraph=paragraph)
 
-@app.route('/update', methods=['POST'])
-def update_content():
-    title = "새로운 제목"
-    paragraph = "새로운 내용입니다! <br> 새로운 텍스트로 시작해 주십시오."
-    return render_template('home.html', title=title, paragraph=paragraph)
-
 @app.route('/result', methods=['POST'])
 def result():
     # 여기서 로직에 따라 결과 페이지를 결정합니다.
